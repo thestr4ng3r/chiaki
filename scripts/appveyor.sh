0971afdabc76dc1dab09bd1fcd2cc98261b85e0f
Florian Märkl <info@florianmaerkl.de>
2019-08-22 20:55:42 +0200

    Enable Unit Tests on AppVeyor (#4)

--- scripts/appveyor.sh
+++ scripts/appveyor.sh
@@ -43,9 +43,11 @@ export PATH="$PWD/protoc/bin:$PATH" || exit 1
 PYTHON="C:/Python37/python.exe"
 "$PYTHON" -m pip install protobuf || exit 1
 
-mkdir build && cd build || exit 1
-
 QT_PATH="C:/Qt/5.12.4/msvc2017_64"
+
+COPY_DLLS="$PWD/openssl-1.1/x64/bin/libcrypto-1_1-x64.dll $PWD/openssl-1.1/x64/bin/libssl-1_1-x64.dll $SDL_ROOT/lib/x64/SDL2.dll"
+
+mkdir build && cd build || exit 1
 
 cmake \
 	-G Ninja \
@@ -61,6 +63,9 @@ cmake \
 	.. || exit 1
 
 ninja || exit 1
+
+test/chiaki-unit.exe || exit 1
+
 cd .. || exit 1
 
 
@@ -72,8 +77,4 @@ mkdir Chiaki && cp build/gui/chiaki.exe Chiaki || exit 1
 # cp build/gui/chiaki.pdb Chiaki
 
 "$QT_PATH/bin/windeployqt.exe" Chiaki/chiaki.exe || exit 1
-cp \
-	openssl-1.1/x64/bin/libcrypto-1_1-x64.dll \
-	openssl-1.1/x64/bin/libssl-1_1-x64.dll \
-	"$SDL_ROOT/lib/x64/SDL2.dll" \
-	Chiaki
+cp -v $COPY_DLLS Chiaki
