d4156fe0d9c7b548f8296c2bf13ea9bff1874afd
Florian Märkl <info@florianmaerkl.de>
2019-08-24 16:22:05 +0200

    FreeBSD Fixes

--- lib/src/ctrl.c
+++ lib/src/ctrl.c
@@ -35,6 +35,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #endif
 
 #define SESSION_OSTYPE "Win10.0.0"
--- lib/src/senkusha.c
+++ lib/src/senkusha.c
@@ -36,6 +36,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #endif
 
 
