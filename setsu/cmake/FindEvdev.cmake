d82c5b435a819ac75db300b9a32c37dba8ab674a
Florian Märkl <florian.maerkl@seitwerk.de>
2020-07-01 22:31:35 +0200

    Build setsu from root (#267)

--- setsu/cmake/FindEvdev.cmake
+++ setsu/cmake/FindEvdev.cmake
@@ -3,18 +3,15 @@
 set(_prefix Evdev)
 set(_target "${_prefix}::libevdev")
 
-find_package(PkgConfig REQUIRED)
+find_package(PkgConfig)
 
-pkg_check_modules("${_prefix}" libevdev)
-
-function(resolve_location)
-
-endfunction()
-
-if(${_prefix}_FOUND)
-	add_library("${_target}" INTERFACE IMPORTED)
-	target_link_libraries("${_target}" INTERFACE ${${_prefix}_LIBRARIES})
-	target_include_directories("${_target}" INTERFACE ${${_prefix}_INCLUDE_DIRS})
+if(PkgConfig_FOUND)
+	pkg_check_modules("${_prefix}" libevdev)
+	if(${_prefix}_FOUND AND NOT TARGET "${_target}")
+		add_library("${_target}" INTERFACE IMPORTED)
+		target_link_libraries("${_target}" INTERFACE ${${_prefix}_LIBRARIES})
+		target_include_directories("${_target}" INTERFACE ${${_prefix}_INCLUDE_DIRS})
+	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
--- setsu/cmake/FindUdev.cmake
+++ setsu/cmake/FindUdev.cmake
@@ -3,18 +3,14 @@
 set(_prefix Udev)
 set(_target "${_prefix}::libudev")
 
-find_package(PkgConfig REQUIRED)
-
-pkg_check_modules("${_prefix}" libudev)
-
-function(resolve_location)
-
-endfunction()
-
-if(${_prefix}_FOUND)
-	add_library("${_target}" INTERFACE IMPORTED)
-	target_link_libraries("${_target}" INTERFACE ${${_prefix}_LIBRARIES})
-	target_include_directories("${_target}" INTERFACE ${${_prefix}_INCLUDE_DIRS})
+find_package(PkgConfig)
+if(PkgConfig_FOUND)
+	pkg_check_modules("${_prefix}" libudev)
+	if(${_prefix}_FOUND AND NOT TARGET "${_target}")
+		add_library("${_target}" INTERFACE IMPORTED)
+		target_link_libraries("${_target}" INTERFACE ${${_prefix}_LIBRARIES})
+		target_include_directories("${_target}" INTERFACE ${${_prefix}_INCLUDE_DIRS})
+	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
