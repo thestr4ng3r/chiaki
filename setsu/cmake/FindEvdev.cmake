6fe60b5826b8b036038115c28d7f4d2c4c449456
Florian Märkl <info@florianmaerkl.de>
2020-10-19 22:01:16 +0200

    Fix Evdev and Udev for old CMake

--- setsu/cmake/FindEvdev.cmake
+++ setsu/cmake/FindEvdev.cmake
@@ -4,13 +4,17 @@ set(_prefix Evdev)
 set(_target "${_prefix}::libevdev")
 
 find_package(PkgConfig)
-
-if(PkgConfig_FOUND)
-	pkg_check_modules("${_prefix}" libevdev)
-	if(${_prefix}_FOUND AND NOT TARGET "${_target}")
-		add_library("${_target}" INTERFACE IMPORTED)
-		target_link_libraries("${_target}" INTERFACE ${${_prefix}_LIBRARIES})
-		target_include_directories("${_target}" INTERFACE ${${_prefix}_INCLUDE_DIRS})
+if(PkgConfig_FOUND AND NOT TARGET ${_target})
+	pkg_check_modules("${_prefix}" libevdev IMPORTED_TARGET)
+	if((TARGET PkgConfig::${_prefix}) AND (NOT CMAKE_VERSION VERSION_LESS "3.11.0"))
+		set_target_properties(PkgConfig::${_prefix} PROPERTIES IMPORTED_GLOBAL ON)
+		add_library(${_target} ALIAS PkgConfig::${_prefix})
+	elseif(${_prefix}_FOUND)
+		add_library(${_target} INTERFACE IMPORTED)
+		set_target_properties(${_target} PROPERTIES
+				INTERFACE_INCLUDE_DIRECTORIES "${${_prefix}_INCLUDE_DIRS}")
+		set_target_properties(${_target} PROPERTIES
+				INTERFACE_LINK_LIBRARIES "${${_prefix}_LIBRARIES}")
 	endif()
 endif()
 
