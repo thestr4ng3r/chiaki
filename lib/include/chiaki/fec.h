9200c0b893033cd13b501a1bf0d32de7f4a97c7b
Florian Märkl <info@florianmaerkl.de>
2020-10-22 23:14:54 +0200

    Align FEC Units by 16

--- lib/include/chiaki/fec.h
+++ lib/include/chiaki/fec.h
@@ -31,7 +31,7 @@ extern "C" {
 
 #define CHIAKI_FEC_WORDSIZE 8
 
-CHIAKI_EXPORT ChiakiErrorCode chiaki_fec_decode(uint8_t *frame_buf, size_t unit_size, unsigned int k, unsigned int m, const unsigned int *erasures, size_t erasures_count);
+CHIAKI_EXPORT ChiakiErrorCode chiaki_fec_decode(uint8_t *frame_buf, size_t unit_size, size_t stride, unsigned int k, unsigned int m, const unsigned int *erasures, size_t erasures_count);
 
 #ifdef __cplusplus
 }
