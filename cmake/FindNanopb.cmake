595c576cf251c6b5d2dc5ddb0afc1464f777064c
Florian Märkl <info@florianmaerkl.de>
2020-10-29 11:39:20 +0100

    Simplify even more

--- cmake/FindNanopb.cmake
+++ cmake/FindNanopb.cmake
@@ -3,9 +3,6 @@
 find_package(nanopb CONFIG)
 find_file(NANOPB_GENERATOR_PY nanopb_generator.py PATH_SUFFIXES bin)
 
-find_path(Nanopb_INCLUDE_DIR NAMES pb_encode.h pb_decode.h)
-find_library(Nanopb_LIBRARY NAMES Nanopb)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Nanopb
 	FOUND_VAR Nanopb_FOUND
@@ -15,12 +12,7 @@ find_package_handle_standard_args(Nanopb
 )
 
 if(Nanopb_FOUND)
-	set(Nanopb_INCLUDE_DIRS "${NANOPB_INCLUDE_DIR}")
 	if(NOT TARGET Nanopb::nanopb)
 		add_library(Nanopb::nanopb ALIAS nanopb::protobuf-nanopb-static)
-#		set_target_properties(nanopb::protobuf-nanopb-static PROPERTIES
-#		  IMPORTED_LOCATION "${Nanopb_LIBRARY}"
-#		  INTERFACE_INCLUDE_DIRECTORIES "${Nanopb_INCLUDE_DIRS}"
-#		)
 	endif()
 endif()
