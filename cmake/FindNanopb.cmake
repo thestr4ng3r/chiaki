0d653def8517186790873af9983535869e69652b
Florian Märkl <florian.maerkl@seitwerk.de>
2020-10-29 11:43:22 +0100

    Remove stray Jerasure stuff in FindNanopb.cmake (#357)

--- cmake/FindNanopb.cmake
+++ cmake/FindNanopb.cmake
@@ -3,9 +3,6 @@
 find_package(nanopb CONFIG)
 find_file(NANOPB_GENERATOR_PY nanopb_generator.py PATH_SUFFIXES bin)
 
-find_path(Jerasure_INCLUDE_DIR NAMES pb_encode.h pb_decode.h)
-find_library(Jerasure_LIBRARY NAMES Jerasure)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Nanopb
 	FOUND_VAR Nanopb_FOUND
@@ -17,9 +14,5 @@ find_package_handle_standard_args(Nanopb
 if(Nanopb_FOUND)
 	if(NOT TARGET Nanopb::nanopb)
 		add_library(Nanopb::nanopb ALIAS nanopb::protobuf-nanopb-static)
-		set_target_properties(Jerasure::Jerasure PROPERTIES
-		  IMPORTED_LOCATION "${Jerasure_LIBRARY}"
-		  INTERFACE_INCLUDE_DIRECTORIES "${Jerasure_INCLUDE_DIRS}"
-		)
 	endif()
 endif()
