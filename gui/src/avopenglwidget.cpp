ecc4bbc1fc866803dc63b023e6388ac0501dd4ea
Blueroom <fred.sund@gmail.com>
2020-11-12 11:41:36 -0800

    Pulled back Original file, no change needed

--- gui/src/avopenglwidget.cpp
+++ gui/src/avopenglwidget.cpp
@@ -147,6 +147,7 @@ AVOpenGLWidget::AVOpenGLWidget(VideoDecoder *decoder, QWidget *parent)
 	frame_uploader_thread = nullptr;
 	frame_fg = 0;
 
+	setMouseTracking(true);
 	mouse_timer = new QTimer(this);
 	connect(mouse_timer, &QTimer::timeout, this, &AVOpenGLWidget::HideMouse);
 	ResetMouseTimeout();
@@ -166,14 +167,9 @@ AVOpenGLWidget::~AVOpenGLWidget()
 }
 
 void AVOpenGLWidget::mouseMoveEvent(QMouseEvent *event)
-{	
+{
 	QOpenGLWidget::mouseMoveEvent(event);
 	ResetMouseTimeout();
-}
-
-void AVOpenGLWidget::resizeEvent(QResizeEvent *event)
-{
-	QWidget::resizeEvent(event);
 }
 
 void AVOpenGLWidget::ResetMouseTimeout()
