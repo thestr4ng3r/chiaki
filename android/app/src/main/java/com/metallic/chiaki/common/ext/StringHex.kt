9c1dfb6974b83e4d58ebfaecd5a19eab5d66961e
Florian Märkl <info@florianmaerkl.de>
2020-12-28 14:03:38 +0100

    Add Sourcehut Reference

--- android/app/src/main/java/com/metallic/chiaki/common/ext/StringHex.kt
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: LicenseRef-AGPL-3.0-only-OpenSSL
-
-package com.metallic.chiaki.common.ext
-
-fun String.hexToByteArray(): ByteArray? = ByteArray(this.length / 2) {
-	this.substring(it * 2, it * 2 + 2).toIntOrNull(16)?.toByte() ?: return null
-}
